22082bc9e9dfba83a9905f3cc97ae7348fc15a34
maurojjzz <maurojim123@gmail.com>
2026-08-02 21:23:14 -0300

    header desktop v1.0

---
+++ src/views/layouts/header.php
@@ -0,0 +1,38 @@
+<header class="container-xxl">
+    <div class="fila_head row">
+        
+        <div class="col-1 d-flex justify-content-center align-items-center">
+            <a class="navbar-brand" href="#">
+                <img class="logo" src="../../../public/img/airlogo.png" alt="Logo de la pagina" >
+            </a>
+        </div>
+        
+        <div class="col  d-flex align-items-center justify-content-center">
+                <ul class="nav navbar-custom rounded-2">
+                    <li class="nav-item active">
+                        <a href="#" class="nav-link">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Vuelos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Promociones</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Novedades</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Ayuda</a>
+                    </li>
+                </ul>
+        </div>
+
+        <div class="col-1 d-flex justify-content-center align-items-center">
+            
+            <button type="button" class="btn  btn-custom">
+                <img src="../../../public/img/icons/perfil .png" alt="Icono de perfil" class="icono_perfil">
+                Ingresar
+            </button>
+        </div>
+    </div>
+</header>
