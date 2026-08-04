eee2d971f90ec83904b59d5f1a01dfc3da89d8b5
maurojjzz <maurojim123@gmail.com>
2026-08-04 15:27:41 -0300

    main page index

--- index.php
+++ index.php
@@ -1,25 +1,37 @@
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aerolinea</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Aerolinea</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="./public/css/index.css">
-    <link rel="stylesheet" href="./public/css/layout/header.css">
-    <link rel="stylesheet" href="./public/css/components/menuMobile.css">
+        <link rel="stylesheet" href="./public/css/index.css">
+        <link rel="stylesheet" href="./public/css/layout/header.css">
+        <link rel="stylesheet" href="./public/css/components/menuMobile.css">
 
-</head>
+    </head>
 
-<body class="container-fluid">
-    <?php require './src/views/layouts/header.php' ?>
+    <body class="container-fluid overflow-x-hidden p-0 m-0">
+        <?php require './src/views/layouts/header.php' ?>
+        
+             <img src="./public/img/clouds.png" class="nubes " alt="fondo de nubes">
 
-    <main>
-    </main>
+             <main class="container-xxl custom-main border border-danger">
+            
+                <div class="hero-content text-center mt-3">
+                    <h5>Vuela sin limites</h5>
+                    <h2>DONDE CADA VIAJE COMIENZA</h2>
+                    <p>Conectamos personas y destinos con una experiencia rapida, segura y confiable.</p>
+                </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</body>
+                <img src="./public/img/airplane.png" alt="Imagen avion" class="hero-plane">
+
+            </main>
+
+       
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </body>
 
 </html>
