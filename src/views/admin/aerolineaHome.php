f69dd837d1ecf36fc5150851de762021af73c6ae
maurojjzz <maurojim123@gmail.com>
2026-08-17 18:55:03 -0300

    sidebar

---
+++ src/views/admin/aerolineaHome.php
@@ -0,0 +1,48 @@
+<?php date_default_timezone_set('America/Argentina/Buenos_Aires');?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Administrador - Aerolínea</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="/public/css/admin.css">
+
+    </head>
+    <body>
+        
+        <div class="d-flex min-vh-100  row g-0 m-0 p-2 border border-danger">
+
+            <aside class="admin-sidebar text-white col-3 col-xxl-2 pe-1">
+                <?php require '../layouts/sidebarAdmin.php'; ?>
+            </aside>
+
+            <div class="admin-main col-9  col-xxl-10 bg-light rounded-3 border border-warning">
+
+                <header class="admin-header">
+                    HEADER
+                </header>
+
+                <main class="container-fluid ">
+
+                    <h1>Panel de Administración</h1>
+                    <p>Gestiona y supervisa todos los aspectos del sistema de reservas.</p>
+
+                    <div class="row g-3">
+                        <div class="col-3">Card</div>
+                        <div class="col-3">Card</div>
+                        <div class="col-3">Card</div>
+                        <div class="col-3">Card</div>
+                    </div>
+
+                </main>
+
+            </div>
+
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ src/views/layouts/sidebarAdmin.php
@@ -0,0 +1,60 @@
+<div class="d-flex flex-column align-items-center m-0 border border-danger h-100 ">
+
+        <div class="d-flex flex-column align-items-center gap-2 py-3 px-3 custom-logo">
+            <img src="../../../public/img/aerologo.webp" alt="logo sitio web" class="logo-sidebar">    
+            <h6>Vuela sin limites</h6>
+        </div>
+
+
+        <ul class="nav navbar-custom d-flex flex-column align-items-center w-100 gap-3 rounded-2 py-3 px-3">
+            <li class="nav-item ">
+                <a class="nav-link rounded-3 w-100" href="#">
+                    <img src="../../../public/img/icons/casa.png" alt="icono casa dashboard" class="iconos-sidebar">
+                    Dashboard
+                </a>
+            </li>
+
+            <li class="nav-item"> 
+                <a class="nav-link active rounded-3" href="#">
+                    <img src="../../../public/img/icons/avionSidebar.png" alt="icono avion seccion admin" class="iconos-sidebar">
+                    Aerolineas
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link rounded-3" href="#">
+                    <img src="../../../public/img/icons/promoSidebar.png" alt="icono promociones seccion admin" class="iconos-sidebar">
+                    Promociones
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link rounded-3" href="#">
+                    <img src="../../../public/img/icons/novedadesSidebar.png" alt="icono novedades seccion admin" class="iconos-sidebar">
+                    Novedades
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link rounded-3" href="#">
+                    <img src="../../../public/img/icons/usuario.png" alt="icono usuarios seccion admin" class="iconos-sidebar">
+                    Usuarios
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link rounded-3" href="#">
+                    <img src="../../../public/img/icons/ceo.png" alt="icono CEOs seccion admin" class="iconos-sidebar">
+                    CEOs
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link rounded-3" href="#">
+                    <img src="../../../public/img/icons/reporteSidebar.png" alt="icono reportes seccion admin" class="iconos-sidebar">
+                    Reportes
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link rounded-3" href="#">
+                    <img src="../../../public/img/icons/configuracionSidebar.png" alt="icono configuraciones seccion admin" class="iconos-sidebar">
+                    Configuraciones
+                </a>
+            </li>
+        </ul>
+</div>
