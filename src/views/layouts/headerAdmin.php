fb10fa10b9c57caf82868d0147f000ea4725ef9f
maurojjzz <maurojim123@gmail.com>
2026-08-23 13:14:12 -0300

    ajustes

--- src/views/layouts/headerAdmin.php
+++ src/views/layouts/headerAdmin.php
@@ -16,13 +16,15 @@
             <img src="../../../public/img/icons/menu-hamburguesa.png" alt="icnno menu hamburguesa" class="logo-header-admin opacity-75"> 
     </button>
 
-    <h5 class="col m-0 p-0">
-        Panel de Administración
+    <h5 class="col-6 col-sm m-0 p-0 ">
+        <span class="d-none d-sm-block">Panel de Administración</span>
+        <span class="d-sm-none ms-3">Administrador</span>
+
     </h5>
 
     <div class="col ctm-user-info d-flex justify-content-end align-items-center gap-2 pe-4 position-relative">
 
-        <button type="button" class="btn btn-bell position-relative p-0 m-0 me-4">
+        <button type="button" class="btn btn-bell position-relative p-0 m-0 me-4 d-none d-sm-block">
             <img src="../../../public/img/icons/bell.png" alt="icono campana notificacion" class="bell-noti opacity-75"> 
             <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
                 1
@@ -34,7 +36,7 @@
             AJ
         </div>
 
-        <div>
+        <div class="d-none d-md-flex flex-column align-items-start justify-content-center gap-0 ctm-user-text">
             <p class="m-0 p-0 ctm-rol">Administrador</p>
             <p class="m-0 p-0 ctm-name">Name LastN</p>
         </div>
